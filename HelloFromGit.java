39472ff2b084ec89aac6ae04b0c375e477b27995
Manoranjan Dubey <58018383+mnmdubey@users.noreply.github.com>
2020-01-14 12:24:06 +0530

    HelloFromGit.java_(v1.01)
    HelloFromGit.java_(v1.01)

---
+++ HelloFromGit.java
@@ -0,0 +1,9 @@
+public class HelloFromGit {
+	
+	public static void main(String [] args){
+		
+		System.out.println("Hello World, This is my First Test. Lets begin the show");
+    System.out.println("This is hello from Git to Jenkins");
+	}
+
+}
